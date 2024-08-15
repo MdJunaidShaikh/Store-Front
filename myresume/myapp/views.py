04fbbaad6e754702f4a69dceaacf5838b12c08ea
MdJunaidShaikh <mshaik30@depaul.edu>
2024-08-15 00:46:22 -0500

    Updated Docker and Terraform
    Commit

--- myresume/myapp/views.py
+++ myresume/myapp/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html');
+
+def about(request):
+    return HttpResponse('about');
+
+def projects(request):
+    return HttpResponse('projects');
+
+def contact(request):
+    return HttpResponse('contact');
